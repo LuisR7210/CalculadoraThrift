ea519f513b0f1a7091800719cfa6310738b46491
LUIS HERNANDEZ <zS17012936@estudiantes.uv.mx>
2020-05-31 01:12:22 -0500

    Falta thrift en servidor

---
+++ Servidor/ThriftCalculadora.thrift
@@ -0,0 +1,6 @@
+service CalculadoraBasica {
+    i32 Sumar( 1: i32 numeroA, 2: i32  numeroB)
+    i32 Restar( 1: i32 numeroA, 2: i32  numeroB )
+    i32 Multiplicar( 1: i32 numeroA, 2: i32  numeroB )
+    i32 Dividir( 1: i32 numeroA, 2: i32  numeroB )
+}
